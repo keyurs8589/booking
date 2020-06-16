f10f18ef42d8f7904eb48dec3e94d37ee8a5721d
keyurs8589 <keyurs8589@gmail.com>
2020-06-16 16:17:33 +1000

    sorting

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useEffect } from 'react';
 import './style/custome.css';
 import BookingSeller from './components/BookingSeller';
 import axios from 'axios';
@@ -9,8 +9,7 @@ function App() {
   const [bookings, setBookings] = useState([]);
   const [products, setProducts] = useState([]);
   const [sellers, setSellers] = useState([]);
-  const [display, setDisplay] = useState([]);
-  const [dis, setDis] = useState([]);
+  const [finalMapped, setFinalMapped] = useState([]);
   const [search, setSearch] = useState('');
 
   useEffect(() => {
@@ -28,114 +27,29 @@ function App() {
       .catch((err) => console.log(err));
   }, []);
 
-  // useEffect(() => {
-  //   console.log('sellter :', sellers);
-  //   console.log('Booking :', bookings);
-  //   console.log('product :', products);
-  //   let result = _.chain(products)
-  //     .groupBy('sellerId')
-  //     .map((value, key) => ({ sellerId: key, users: value }))
-  //     .value();
-  //   console.log('result :', result);
-  // });
-
-  // useEffect(() => {
-  //   setDisplay(
-  //     sellers.map((seller) => ({
-  //       sellerName: seller.name,
-  //       bookingData: products
-  //         .map(
-  //           (product) =>
-  //             seller.id === product.sellerId &&
-  //             bookings
-  //               .map(
-  //                 (booking) =>
-  //                   product.id === booking.productId && {
-  //                     bookingId: booking.id,
-  //                     productName: product.name,
-  //                     bookingQuantity: booking.quantity,
-  //                     productRate: product.rate,
-  //                     cost: (booking.quantity * product.rate) / 100000,
-  //                     bookingStartDate: booking.startDate,
-  //                     bookingEndDate: booking.endDate,
-  //                   }
-  //               )
-  //               .filter((a) => a !== false)
-  //         )
-  //         .filter((a) => a !== false),
-  //     }))
-  //   );
-  // }, [bookings, products, sellers]);
-
   useEffect(() => {
     let mappedBooking = [];
     mappedBooking = bookings.map((booking) => {
       booking.product = products.find(
         (product) => product.id === booking.productId
       );
-      console.log(sellers);
-      sellers.map((seller) => {
-        if (seller.id === booking.product.sellerId) {
-          booking.sellerId = seller.id;
-          booking.sellerName = seller.name;
-        }
-        return booking;
-      });
+      booking.seller = sellers.find(
+        (seller) => seller.id === booking.product.sellerId
+      );
       return booking;
-      // booking.seller = sellers.find(
-      //   (seller) => seller.id === booking.product.sellerId
-      // );
     });
-    let result = _.chain(mappedBooking)
-      .groupBy('sellerId')
-      .map((value, key) => ({ sellerId: key, users: value }))
+    mappedBooking = _.chain(mappedBooking)
+      .groupBy('seller.id')
+      .map((value, key) => ({ sellerId: key, sellerBooking: value }))
       .value();
-    console.log(result);
-    // console.log(mappedBooking);
+    mappedBooking = mappedBooking.map((mapBook) => {
+      mapBook.sellerBooking = mapBook.sellerBooking.sort(
+        (a, b) => new Date(a.startDate) - new Date(b.startDate)
+      );
+      return mapBook;
+    });
+    setFinalMapped(mappedBooking);
   }, [bookings, products, sellers]);
-
-  // const disMemo = useMemo(() => {
-  // setDisplay(
-  //   sellers.map((seller) => {
-  //     const mappedSeller = seller;
-  //     const sellerProduct = products.filter(
-  //       (product) => product.sellerId === seller.id
-  //     );
-  //     const sellerBooking = bookings.filter((booking) => {
-  //       sellerProduct.find((product) => product.id === booking.productId);
-  //     });
-  //     mappedSeller.products = sellerProduct;
-  //     mappedSeller.bookings = sellerBooking;
-  //     return mappedSeller;
-  //   })
-  // );
-
-  // setDisplay(
-  //   sellers.map((seller) =>
-  //     products
-  //       .map(
-  //         (product) =>
-  //           seller.id === product.sellerId && {
-  //             sellerName: seller.name,
-  //             bookingData: bookings
-  //               .map(
-  //                 (booking) =>
-  //                   product.id === booking.productId && {
-  //                     bookingId: booking.id,
-  //                     productName: product.name,
-  //                     bookingQuantity: booking.quantity,
-  //                     productRate: product.rate,
-  //                     cost: (booking.quantity * product.rate) / 100000,
-  //                     bookingStartDate: booking.startDate,
-  //                     bookingEndDate: booking.endDate,
-  //                   }
-  //               ).filter((a) => a !== false),
-  //           }
-  //       ).filter((a) => a !== false)
-  //   )
-  // );
-  // }, [bookings, products, sellers]);
-  // console.log(display);
 
   return (
     <div className='roboto body-margin'>
@@ -145,7 +59,7 @@ function App() {
         <Search search={search} setSearch={setSearch} />
       </div>
       <div>
-        <BookingSeller display={display} />
+        <BookingSeller finalMapped={finalMapped} sellers={sellers} />
       </div>
     </div>
   );
--- src/components/BookingSeller.jsx
+++ src/components/BookingSeller.jsx
@@ -1,33 +1,57 @@
-import React from "react";
-import BookingNotActive from "./BookingNotActive";
-import Booking from "./Booking";
+import React from 'react';
+import BookingNotActive from './BookingNotActive';
+import Booking from './Booking';
+import moment from 'moment';
 
-function BookingSeller({ display }) {
-  
+function BookingSeller({ finalMapped, sellers }) {
+  let today = moment(new Date()).format('DD-MM-YYYY');
   return (
-    <div className="mb-3">
-      {display.map((dis, index) => (
-        <div className="my-4" key={index}>
-          <h4 className="seller-title">{dis.sellerName}</h4>
-          {dis.bookingData.length === 0 ? (
-            <BookingNotActive message="No Bookings."/>
+    <div className='mb-3'>
+      {sellers.map((seller, index) => (
+        <div className='my-4' key={index}>
+          <h4 className='seller-title'>{seller.name}</h4>
+          {finalMapped.find((final) => final.sellerId === seller.id) ? (
+            finalMapped
+              .find((final) => final.sellerId === seller.id)
+              .sellerBooking.find(
+                (booking) =>
+                  moment(booking.bookingEndDate).format('DD-MM-YYYY') >= today
+              ) ? (
+              <div>
+                <Booking
+                  booking={{
+                    bookingId: 'ID',
+                    productName: 'Product Name',
+                    bookingQuantity: 'Quantity',
+                    productRate: 'Rate',
+                    cost: 'Cost',
+                    bookingStartDate: 'Start Date',
+                    bookingEndDate: 'End Date',
+                  }}
+                />
+                {finalMapped
+                  .find((final) => final.sellerId === seller.id)
+                  .sellerBooking.map((booking, index) => (
+                    <Booking
+                      key={index}
+                      booking={{
+                        bookingId: booking.id,
+                        productName: booking.product.name,
+                        bookingQuantity: booking.quantity,
+                        productRate: booking.product.rate,
+                        cost:
+                          (booking.quantity * booking.product.rate) / 100000,
+                        bookingStartDate: booking.startDate,
+                        bookingEndDate: booking.endDate,
+                      }}
+                    />
+                  ))}
+              </div>
+            ) : (
+              <BookingNotActive message='No Active Booking.' />
+            )
           ) : (
-            <div>
-              <Booking
-                booking={{
-                  bookingId: "ID",
-                  productName: "Product Name",
-                  bookingQuantity: "Quantity",
-                  productRate: "Rate",
-                  cost: "Cost",
-                  bookingStartDate: "Start Date",
-                  bookingEndDate: "End Date",
-                }}
-              />
-              {dis.bookingData.map((book) =>
-                book.map((b, index) => <Booking key={index} booking={b} />)
-              )}
-            </div>
+            <BookingNotActive message='No Booking.' />
           )}
         </div>
       ))}
